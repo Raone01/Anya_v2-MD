3bcc718543f93f1213873c3cbc51eb9fc088617a
∆~•Sᴀɴᴀᴛᴀɴɪ Aᴅɪᴛʏᴀ•~∆ <121045125+Raone01@users.noreply.github.com>
2023-10-22 09:15:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -121,10 +121,10 @@ module.exports = {
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
   sessionId: process.env.SESSION_ID || "_AN_YA_HoledEarplugs", 
-  auto_read_status : process.env.AUTO_READ_STATUS || "true",
-  alwaysonline: process.env.ALWAYS_ONLINE || "true", 
-  readmessage: process.env.READ_MESSAGE || "true", 
-  auto_status_saver: process.env.AUTO_STATUS_SAVER || "true",
+  auto_read_status : process.env.AUTO_READ_STATUS || "false",
+  alwaysonline: process.env.ALWAYS_ONLINE || "false", 
+  readmessage: process.env.READ_MESSAGE || "false", 
+  auto_status_saver: process.env.AUTO_STATUS_SAVER || "false",
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
