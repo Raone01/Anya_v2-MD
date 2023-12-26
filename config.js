56981eb4e20b5755efd10bb23871d9fd787966eb
∆~•Sᴀɴᴀᴛᴀɴɪ Aᴅɪᴛʏᴀ•~∆ <121045125+Raone01@users.noreply.github.com>
2023-12-26 22:38:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -63,18 +63,18 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@PikaBotz",
-  packname: process.env.PackName || "Queen Anya v2 MD",
+  botname: process.env.BotName || "∆~•Ꮎɱι-Ᏸσт•~∆", 
+  author: process.env.Author || "SILENT.XARMONY",
+  packname: process.env.PackName || "∆~•Ꮎᴍɪ•~∆",
   socialLink: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
+  footer: process.env.Footer || "© ∆~•Ꮎɱι-Ᏸσт•~∆",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
-  instagramId: process.env.Insta || "8.08_only_mine",
+  ownername: process.env.Owner_Name || "∆~•Sᴀɴᴀᴛᴀɴɪ Aᴅɪᴛʏᴀ•~∆",
+  ownernumber: process.env.Owner_Number || "919634945456",
+  instagramId: process.env.Insta || "SILENT.XARMONY",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+  mongoUrl: process.env.MongoDB || "mongodb+srv://debopi8523:aaaa1111aaaa@cluster0.9s0bfxq.mongodb.net/?retryWrites=true&w=majority",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
@@ -105,18 +105,18 @@ module.exports = {
 
 
 // Ignore them 👇🏻
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@PikaBotz" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.botname = process.env.BotName || "∆~•Ꮎɱι-Ᏸσт•~∆" 
+global.author = process.env.Author || "@SILENT.XARMONY" 
+global.packname = process.env.PackName || "∆~•Ꮎᴍɪ•~∆" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
-global.prefa = process.env.Prefix || ['-'] 
+global.footer = process.env.Footer || "© ∆~•Ꮎɱι-Ᏸσт•~∆" 
+global.prefa = process.env.Prefix || ['.'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
-global.adress = process.env.Continent || "Asia, India, Assam" 
+global.ownername = process.env.Owner_Name || "∆~•Sᴀɴᴀᴛᴀɴɪ Aᴅɪᴛʏᴀ•~∆" 
+global.ownernumber = process.env.Owner_Number || "919634945456" 
+global.adress = process.env.Continent || "Asia, India, Uttar Pradesh" 
 global.timezone = process.env.TimeZone || "Asia/Kolkata" 
-global.instagramId = process.env.Insta || "8.08_only_mine" 
+global.instagramId = process.env.Inst || "SILENT.XARMONY"
 global.email = process.env.Email_Id || "example@example.com" 
   
 //--------------- Tip ----------------\\
